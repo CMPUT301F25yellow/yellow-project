9f4c032a10fd6b5173c8b6b0b880b235287bbfa8
Tabrez Anwar <anwartabrez01@gmail.com>
2025-11-04 19:38:08 -0700

    Tapping on the profile icon in the home screen takes you to the profile activity

--- code/app/src/main/java/com/example/yellow/ui/ProfileAdminFragment.java
+++ code/app/src/main/java/com/example/yellow/ui/ProfileAdminFragment.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.yellow.R;
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class ProfileAdminFragment extends Fragment {
@@ -25,7 +23,7 @@ public class ProfileAdminFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_profile_admin, container, false);
+        return inflater.inflate(R.layout.fragment_profile_admin, container, false);
     }
 
     @Override
